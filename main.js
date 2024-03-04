57d5957a4c745b5899c9cde574af3f02420e51c4
Eric Ihle <zekeotronic@gmail.com>
2024-03-04 01:05:49 -0500

    adds active states and js

---
+++ main.js
@@ -0,0 +1,45 @@
+let rating = 0;
+const one = document.getElementById('rate-1');
+
+function toggleActive(e) {
+  const btn = e.target;
+  const btnValue = parseInt(btn.getAttribute('data-value'));
+
+  if (btnValue === rating) {
+    rating = 0;
+  } else {
+    rating = btnValue;
+  }
+
+  ratingBtns.forEach((btn, i) => {
+    if (i === rating - 1) {
+      btn.previousElementSibling.classList.add('active');
+      btn.parentElement.classList.add('active');
+    } else {
+      btn.previousElementSibling.classList.remove('active');
+      btn.parentElement.classList.remove('active');
+    }
+  });
+  console.log("Rating",rating);
+}
+
+function toggleHover(e) {
+  const btn = e.target;
+  btn.previousElementSibling.classList.toggle('hover');
+  btn.parentElement.classList.toggle('hover');
+}
+
+
+
+const ratingBtns = document.querySelectorAll('.rating-btn');
+
+const submitBtn = document.getElementById('submit-btn');
+
+ratingBtns.forEach(btn => {
+  btn.addEventListener('click', toggleActive);
+  btn.addEventListener('mouseover', toggleHover);
+  btn.addEventListener('mouseout', toggleHover);
+})
+
+submitBtn.addEventListener('mouseover', toggleHover);
+submitBtn.addEventListener('mouseout', toggleHover);
